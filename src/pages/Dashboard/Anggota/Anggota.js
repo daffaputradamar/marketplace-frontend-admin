6591d7b1ca537b228efafcb2afa20985a342bc13
Daffa Akbar <daffaputradamar@gmail.com>
2019-11-07 12:24:54 +0700

    add daftar anggota

--- src/pages/Dashboard/Anggota/Anggota.js
+++ src/pages/Dashboard/Anggota/Anggota.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import { Table } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Table, Segment, Dimmer, Loader, Image } from "semantic-ui-react";
+import axios from "axios";
+import { HOSTNAME } from "../../../App";
 
 function Anggota() {
-	return (
-		<Table fixed celled textAlign="center">
-			<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell>Username</Table.HeaderCell>
-					<Table.HeaderCell>Tanggal Terdaftar</Table.HeaderCell>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				<Table.Row>
-					<Table.Cell>dataUsername</Table.Cell>
-					<Table.Cell>dataTanggal</Table.Cell>
-				</Table.Row>
-			</Table.Body>
-		</Table>
-	);
+  const [kumpulanPengguna, setkumpulanPengguna] = useState([]);
+  const [kumpulanAnggota, setKumpulanAnggota] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`${HOSTNAME}/pengguna`).then(res => {
+      setkumpulanPengguna(res.data);
+      const anggotas = res.data.filter(anggota => anggota.keanggotaan);
+      setKumpulanAnggota(anggotas);
+      setLoading(false);
+    });
+  }, []);
+
+  return loading ? (
+    <Segment>
+      <Dimmer active inverted>
+        <Loader inverted content="Loading" />
+      </Dimmer>
+
+      <Image src="/images/wireframe/short-paragraph.png" />
+    </Segment>
+  ) : (
+    <Table fixed celled textAlign="center">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell width="2">Id</Table.HeaderCell>
+          <Table.HeaderCell>Nama</Table.HeaderCell>
+          <Table.HeaderCell>Username</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {kumpulanAnggota.map(anggota => {
+          return (
+            <Table.Row>
+              <Table.Cell>{anggota.id_pengguna}</Table.Cell>
+              <Table.Cell>{anggota.nama}</Table.Cell>
+              <Table.Cell>{anggota.username}</Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  );
 }
 
 export default Anggota;
--- src/pages/Dashboard/Anggota/KonfirmasiAnggota.js
+++ src/pages/Dashboard/Anggota/KonfirmasiAnggota.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Table, Button } from 'semantic-ui-react';
+import React from "react";
+import { Table, Button } from "semantic-ui-react";
 
 function Anggota() {
-	return (
-		<Table fixed celled textAlign="center">
-			<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell>Username</Table.HeaderCell>
-					<Table.HeaderCell>Bukti Bayar</Table.HeaderCell>
-					<Table.HeaderCell>Action</Table.HeaderCell>
-				</Table.Row>
-				<Table.Row>
-					<Table.Cell>dataUsername</Table.Cell>
-					<Table.Cell>dataBukti</Table.Cell>
-					<Table.Cell>
-						<Button basic color="green">
-							Konfirmasi
-						</Button>
-					</Table.Cell>
-				</Table.Row>
-			</Table.Header>
-		</Table>
-	);
+  return (
+    <Table fixed celled textAlign="center">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell width="2">Id</Table.HeaderCell>
+          <Table.HeaderCell>Nama</Table.HeaderCell>
+          <Table.HeaderCell>Bukti Bayar</Table.HeaderCell>
+          <Table.HeaderCell>Action</Table.HeaderCell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Id</Table.Cell>
+          <Table.Cell>Tes Nama</Table.Cell>
+          <Table.Cell>dataBukti</Table.Cell>
+          <Table.Cell>
+            <Button basic color="green">
+              Konfirmasi
+            </Button>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Header>
+    </Table>
+  );
 }
 
 export default Anggota;
